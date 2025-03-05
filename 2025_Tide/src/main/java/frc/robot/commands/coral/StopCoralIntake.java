1eb12f43578a6d7a8bd84edf9db38dccfebfa5f8
thecalebcarter <46938807+thecalebcarter@users.noreply.github.com>
2025-03-04 23:57:45 -0500

    Whoops!

--- 2025_Tide/src/main/java/frc/robot/commands/coral/StopCoralIntake.java
+++ 2025_Tide/src/main/java/frc/robot/commands/coral/StopCoralIntake.java
@@ -1,9 +1,7 @@
 package frc.robot.commands.coral;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
-import frc.robot.commands.algae.StopAlgaeIntake;
 
 public class StopCoralIntake extends Command {
 	private boolean firstSensorSeesCoral = false;
